561a5314270ca220051395a0d442705d541fd688
Erik Pak <erikpak@live..com>
2025-11-03 11:55:22 -0600

    Add sentiment analyzer HTML interface and Python version checker script
    - Implemented a web-based sentiment analyzer using HTML, Tailwind CSS, and Firebase for authentication.
    - Added functionality to analyze text sentiment as positive, negative, or neutral with a user-friendly interface.
    - Included a loading spinner and result display with appropriate styling based on sentiment.
    - Created a Python script to print the versions of key libraries used in the project, including sentence-transformers, PyTorch, and Transformers.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# SentenceTransformer
+# Fine-Tune Sentence Transformer
+
+- Python 3.13.9
+- pip install torch sentence-transformers ipywidgets datasets pymupdf langchain-text-splitters
+
+## Contrastive Learning
---
+++ Src/test.py
@@ -0,0 +1,11 @@
+from importlib.metadata import version
+import sys
+
+print("SentenceTransformer Version:", version("sentence-transformers"))
+print("PyTorch Version:", version("torch"))
+print("Transformers Version:", version("transformers"))
+print("Tokenizers Version:", version("tokenizers"))
+print("Datasets Version:", version("datasets"))
+print("Numpy Version:", version("numpy"))
+print("Python Version:", sys.version)
+
